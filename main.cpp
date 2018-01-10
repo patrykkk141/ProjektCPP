dd88eb10f929ad699ad0b97a0ab46b2ecc47cd24
patryk <patryk.iwanczyk0102@gmail.com>
2018-01-10 09:20:12 +0100

    Dodanie menu

--- main.cpp
+++ main.cpp
@@ -6,6 +6,16 @@ using namespace std;
 
 int main() {
 
+    cout<<"***KALKULATOR MACIERZOWY***"
+            "\nOperatory: "
+            "\n+ - Dodawanie (liczba lub macierz)"
+            "\n- - Odejmowanie (liczba lub macierz) "
+            "\n* - Mnozenie (liczba lub macierz) "
+            "\n! - Macierz transponowana (operator lewostronny)"
+            "\n~ - Wyznczanik macierzy (operator lewostronny)"
+            "\n@ - Macierz dopelnien algebraicznych (operator lewostronny) "
+            "\n^ - Macierz odwrotna (operator lewostronny)";
+
     Interfejs intf;
     intf.CommandLineInterface();
 
